62e30e6a2a9aad887f00168203f1c31f42d5e020
danielpmc <danielpd93@gmail.com>
2019-04-07 17:15:06 +0100

    Fixed a few things. Removed admin command

--- commands/achievement.js
+++ commands/achievement.js
@@ -3,7 +3,7 @@ exports.run = async (client, message, args, Discord) => {
        const text = args.join(" ");
         if (text === null) return message.channel.send("You need to provide text for the achievement");
             if (text.length > 25) return message.reply('text must be under 25 characters.');
-        const superagent = require('superagent')
+        const superagent = require('superagent');
         const { body } = await superagent
             .get('https://www.minecraftskinstealer.com/achievement/a.php')
             .query({
