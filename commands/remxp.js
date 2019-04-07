f2ff0ac12493ee96640a332c9c928b08fc354ed5
danielpmc <danielpd93@gmail.com>
2019-04-07 14:42:52 +0100

    Create remxp.js

---
+++ commands/remxp.js
@@ -0,0 +1,15 @@
+const sql = require("sqlite");
+sql.open("./SQL/settings/guildsettings.sqlite");
+exports.run = (client, message, args, Discord) => {
+    const user = message.mentions.users.first();
+     if (message.mentions.users.size < 1) return message.channel.send("Need to tag a user to take xp from them.");
+     const amount = parseInt(args[1]);
+     if (amount < 1) return message.channel.send("Didnt provide any money to remove.");
+     if (!message.member.hasPermission("MANAGE_GUILD")) return message.channel.send("Need MANAGE_GUILD permission to use this command.")
+    sql.get(`SELECT * FROM profiles WHERE guildId ="${message.guild.id}" AND userId ="${user.id}"`).then(row => {
+     if (!row) message.channel.send("User needs to start talking first.")
+        if (isNaN(amount)) return message.channel.send("Not a valid number")
+        sql.run(`UPDATE profiles SET xp = ${row.xp -= amount} WHERE guildId ="${message.guild.id}" AND userId = ${user.id}`);
+		message.channel.send("I have taken xp from: " + user.username + " " + amount + "XP");
+    })
+}
