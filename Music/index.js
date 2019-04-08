ff3b24b43a2a6123e55ebd3249455138f269d621
danielpmc <danielpd93@gmail.com>
2019-04-08 19:18:31 +0100

    Fuck it for now. I cant find da wae...

--- Music/index.js
+++ Music/index.js
@@ -12,16 +12,17 @@ client.on('error', err => client.channels.get("544290801216126976").send('[ERROR
 client.on('reconnecting', () => client.channels.get("544290801216126976").send('Got disconnected from discord : Reconnecting...'));
 
 const sql = require("sqlite");
-sql.open("../SQL/settings/guildsettings.sqlite");
 client.on('message', async msg => {
-	sql.get(`SELECT * FROM scores WHERE guildId ="${message.guild.id}"`).then(row => {
+	sql.open("../SQL/settings/guildsettings.sqlite");
+	sql.get(`SELECT * FROM scores WHERE guildId ="${msg.guild.id}"`).then(row => {
 	if (msg.author.bot) return undefined;
 	if (!msg.content.startsWith(row.prefix)) return undefined;
 	const args = msg.content.split(' ');
 	const searchString = args.slice(1).join(' ');
 	const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
 	const serverQueue = queue.get(msg.guild.id);
-	let command = msg.content.toLowerCase().split(' ')[0];
+})
+	const command = msg.content.toLowerCase().split(' ')[0];
 	command = command.slice(row.prefix.length)
 	if (command === 'play') {
 		const voiceChannel = msg.member.voiceChannel;
@@ -194,6 +195,7 @@ return msg.channel.send(queueembed)
 	}
 
 	return undefined;
+	
 });
 
 async function handleVideo(video, msg, voiceChannel, playlist = false) {
@@ -284,6 +286,6 @@ var pleyembed = new RichEmbed()
 
 	serverQueue.textChannel.send(pleyembed);
 
-}})
+}
 
 client.login(config.token);
